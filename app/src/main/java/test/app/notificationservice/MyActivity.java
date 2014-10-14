4124101613578f5c43904bc5c674bafe34e6d91b
Fernando Bernal <fernando.abernalm@gmail.com>
2014-10-14 15:32:07 -0700

    Second Commit - Added notifications when service start and method for listening notification when is incoming an when is focused

--- app/src/main/java/test/app/notificationservice/MyActivity.java
+++ app/src/main/java/test/app/notificationservice/MyActivity.java
@@ -1,40 +1,22 @@
 package test.app.notificationservice;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MyActivity extends ActionBarActivity {
+public class MyActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-
-        Intent intent = new Intent(this, NotifierService.class);
-        startService(intent);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/test/app/notificationservice/NotifierService.java
+++ app/src/main/java/test/app/notificationservice/NotifierService.java
@@ -2,6 +2,12 @@ package test.app.notificationservice;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
@@ -12,10 +18,25 @@ public class NotifierService extends AccessibilityService {
 
     static final String TAG = "notification-Service";
 
+    NotificationManager notificationManager = (NotificationManager)
+            getSystemService(NOTIFICATION_SERVICE);
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         Log.i(TAG, "onAccessibilityEvent");
-        Log.i(TAG, event.toString());
+        CharSequence packageName = event.getPackageName();
+
+        // Identify the event and package for the notifications incoming.
+        switch (event.getEventType()){
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                // Cancel alert.
+                notificationManager(packageName, false);
+                break;
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                // Initiate alert.
+                notificationManager(packageName, true);
+        }
+
     }
 
     @Override
@@ -27,9 +48,41 @@ public class NotifierService extends AccessibilityService {
     protected void onServiceConnected() {
         Log.i(TAG,"onServiceConnected");
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED + AccessibilityEvent.TYPE_VIEW_FOCUSED;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
         info.notificationTimeout = 100;
         setServiceInfo(info);
+
+        Notification notification = configureNotifications();
+
+        notificationManager.notify(0, notification);
+    }
+
+    @Override
+    public void onDestroy(){
+        notificationManager.cancel(0);
+    }
+
+    // Parameter for notifications when the service is started.
+    private Notification configureNotifications(){
+        Resources r = getResources();
+
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this,MyActivity.class), 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker(r.getString(R.string.notification_description))
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(r.getString(R.string.notif_tittle_text))
+                .setContentText(r.getString(R.string.notif_contento_text))
+                .setContentIntent(pi)
+                .setAutoCancel(false)
+                .build();
+
+        return notification;
+    }
+
+    // Method for management notification
+    private void notificationManager(CharSequence pachageName, boolean incomingNotification){
+        // Code pending
     }
 }
