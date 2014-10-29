db8189bdd68ef870a3ac45fdd2d995cb892afd71
Fernando Bernal <fernando.abernalm@gmail.com>
2014-10-29 14:05:40 -0700

    Four Commit - Add conection with Firebase

--- app/src/main/java/test/app/notificationservice/MyActivity.java
+++ app/src/main/java/test/app/notificationservice/MyActivity.java
@@ -1,14 +1,24 @@
 package test.app.notificationservice;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.firebase.client.Firebase;
 
 public class MyActivity extends Activity{
+
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        MyActivity.context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return MyActivity.context;
     }
 }
--- app/src/main/java/test/app/notificationservice/NotifierService.java
+++ app/src/main/java/test/app/notificationservice/NotifierService.java
@@ -10,48 +10,64 @@ import android.content.res.Resources;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.MutableData;
+import com.firebase.client.Transaction;
 
 /**
  * Created by Carolina on 03/10/2014.
  */
 public class NotifierService extends AccessibilityService {
 
-    static final String TAG = "notification-Service";
+    static final String TAG = "NotificationService";
+    String packageName;
+    UserAlert userAlert;
 
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.i(TAG, "onAccessibilityEvent");
-        CharSequence packageName = event.getPackageName();
+    public void onCreate(){
+        Log.i(TAG, "onCreate");
 
-        // Identify the event and package for the notifications incoming.
-        switch (event.getEventType()){
-            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
-                // Cancel alert.
-                notificationManager(packageName, false);
-                break;
-            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-                // Initiate alert.
-                notificationManager(packageName, true);
-        }
-
-    }
-
-    @Override
-    public void onInterrupt() {
-        Log.i(TAG,"onInterrupt");
+        userAlert = new UserAlert();
     }
 
     @Override
     protected void onServiceConnected() {
         Log.i(TAG,"onServiceConnected");
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED + AccessibilityEvent.TYPE_VIEW_FOCUSED;
+        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
         info.notificationTimeout = 100;
         setServiceInfo(info);
 
         // Show notification when service is started
         showNotification(true);
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.i(TAG, "onAccessibilityEvent");
+
+        packageName = event.getPackageName().toString();
+
+        // Identify event and package for the notifications incoming.
+        switch (event.getEventType()){
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                // Initiate alert.
+
+                userAlert.startAlert(packageName);
+
+                Log.i(TAG, "MensajeEntrante " + event.toString());
+                break;
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.i(TAG,"onInterrupt");
     }
 
     @Override
@@ -83,11 +99,5 @@ public class NotifierService extends AccessibilityService {
             notificationManager.notify(0, notification);
         else
             notificationManager.cancel(0);
-    }
-
-
-    // Method for management notification
-    private void notificationManager(CharSequence pachageName, boolean incomingNotification){
-        // Code pending
     }
 }
